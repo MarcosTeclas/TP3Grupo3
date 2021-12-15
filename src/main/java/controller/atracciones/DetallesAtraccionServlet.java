83ca020541a759587b302ce63ada4c31a133dc33
Yane05 <yane.05@hotmail.com>
2021-12-15 00:45:08 -0300

    agrego servlet de detalles

--- src/main/java/controller/atracciones/DetallesAtraccionServlet.java
+++ src/main/java/controller/atracciones/DetallesAtraccionServlet.java
@@ -8,8 +8,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Atraccion;
-import model.TipoDeAtraccion;
 import services.atracciones.AtraccionService;
 
 @WebServlet("/detalles.do")
@@ -19,21 +17,26 @@ public class DetallesAtraccionServlet extends HttpServlet {
 	private static final long serialVersionUID = 7673388948368765381L;
 	private AtraccionService atraccionService;
 
-	@Override
+	/*@Override
 	public void init() throws ServletException {
 		super.init();
 		this.atraccionService = new AtraccionService();
-	}
+	}*/
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		Integer id = Integer.parseInt(request.getParameter("id"));
+		
+		String detalles = atraccionService.find(id).getDetalle();
+		request.setAttribute("detalles", detalles);
 
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/view/atracciones/detalles.jsp");
 		dispatcher.forward(request, response);
 	}
 
-	@Override
+	/*@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String nombre = request.getParameter("nombre");
@@ -53,6 +56,6 @@ public class DetallesAtraccionServlet extends HttpServlet {
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/view/atracciones/create.jsp");
 			dispatcher.forward(request, response);
 		}
-	}
+	}*/
 
 }
